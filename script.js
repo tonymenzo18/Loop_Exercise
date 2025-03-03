1d107654d387bdf5d356b4a1775dc335117cf607
tonymenzo18 <anthonynemenzo181@gmail.com>
2025-03-03 09:44:15 +0800

    final

---
+++ script.js
@@ -0,0 +1,41 @@
+// TASK 1
+// let num = 5;
+
+// for (let i = 1; i <= 10; i++) 
+//     {
+//         console.log(`${num} x ${i} = ${num * i}`);
+//     }
+
+// TASK 2
+// let n = 5; 
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     sum = sum + i;
+// }
+// console.log(`The sum of the first ${n} numbers is: ${sum}`);
+
+// TASK 3
+// let arr = [2, 4, 6, 8, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(`Array Element: ${arr[i]}`);
+// }
+
+// TASK 4
+// let n = 5; 
+
+// for (let i = 1; i <= n; i++) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//         row = row + "*"; 
+//     }
+//     console.log(row); 
+// }
+
+// TASK 5
+let arr = [1, 3, 5, 7, 9];
+
+for (i = 1; i <= arr.length; i++) {
+    console.log(`Reversed Element: ${arr[arr.length - i]}`);
+}
